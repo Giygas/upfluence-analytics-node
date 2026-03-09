32ceade77222c01ac27cd0912d725858bb95d945
Giygas <gustavorossich@gmail.com>
2026-03-09 20:41:43 +0100

    Prettier formatting

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import Koa from "Koa";
 const app = new Koa();
 
 app.use(async (ctx) => {
-  if (ctx.method != "GET") {
-    ctx.status = 405;
-    ctx.body = "Method not allowed";
-    return;
-  }
-  if (ctx.path != "/analytics") {
-    ctx.status = 404;
-    ctx.body = "Invalid path";
-    return;
-  }
+    if (ctx.method != "GET") {
+        ctx.status = 405;
+        ctx.body = "Method not allowed";
+        return;
+    }
+    if (ctx.path != "/analytics") {
+        ctx.status = 404;
+        ctx.body = "Invalid path";
+        return;
+    }
 
-  ctx.body = "Hello World";
+    ctx.body = "Hello World";
 });
 
 app.listen(3000);
